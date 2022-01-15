b48615e88148150eb30d3c7efa5ed7cf1221dde9
Fabian Volkers <fabian.volkers@gmail.com>
2022-01-15 18:40:00 +0100

    Add get_services method

--- upptime-exporter/api.py
+++ upptime-exporter/api.py
@@ -1,7 +1,14 @@
-from github import GitHub
+from github import Github
 
 import settings
 
-g = GitHub(settings.GH_ACCESS_TOKEN)
+g = Github(settings.GH_ACCESS_TOKEN)
+repo = g.get_repo(settings.UPPTIME_REPO_NAME)
 
+def get_services():
+    services = repo.get_dir_contents('/api')
+    return services
+    print(services)
+    for service in services:
 
+        print(service.name)
